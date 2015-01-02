24a91b03a152d34c590bd663d23700a377506d5e
Jawan81 <deadzac@gmx.net>
2015-01-02 16:08:05 +0100

    Added Tile class

---
+++ js/tile.js
@@ -0,0 +1,8 @@
+var Tile = function(xCoord, yCoord) {
+//    this.xCoord = xCoord;
+//    this.yCoord = yCoord;
+    this.id = xCoord.toString() + yCoord.toString();
+    this.centerX = 101.0 * xCoord - 50.5;
+    this.centerY = 83.0 * yCoord - 41.5;
+};
+
