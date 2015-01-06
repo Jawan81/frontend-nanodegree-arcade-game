5651169383a7c2a49fa2286abfcc27fe4f2652ad
Jawan81 <deadzac@gmx.net>
2015-01-06 12:01:39 +0100

    Added Gem

---
+++ js/gem.js
@@ -0,0 +1,44 @@
+var Gem = function(sprite, tile, score) {
+    Actor.call(this, sprite, tile, 20);
+    this.picked = false;
+    this.score = score;
+};
+
+Gem.prototype = Object.create(Actor.prototype);
+Gem.constructor = Gem;
+
+Gem.prototype.pick = function() {
+    this.picked = true;
+};
+
+Gem.prototype.isPicked = function(player) {
+    if (! this.picked) {
+        return this.collidesWith(player);
+    }
+
+    return false;
+};
+
+Gem.prototype.checkPicked = function(player) {
+    var picked = this.isPicked(player);
+
+    if (picked) {
+        this.hide();
+        this.pick();
+    }
+
+    return picked;
+};
+
+Gem.prototype.getScore = function() {
+    if (this.picked) {
+        return this.score;
+    }
+
+    return 0;
+};
+
+Gem.prototype.reset = function() {
+    this.picked = false;
+};
+
